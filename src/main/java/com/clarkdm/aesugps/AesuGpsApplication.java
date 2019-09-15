43c51d945b602bfe6485565da0063a90b29f3dbe
clarkdm <clarkdm@users.noreply.github.com>
2019-09-15 16:10:30 +0100

    start

---
+++ src/main/java/com/clarkdm/aesugps/AesuGpsApplication.java
@@ -0,0 +1,13 @@
+package com.clarkdm.aesugps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesuGpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AesuGpsApplication.class, args);
+    }
+
+}
